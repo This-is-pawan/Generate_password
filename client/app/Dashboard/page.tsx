d65ce0e5d8c75a9c63908d11ebb5237393e04a8a
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-10-18 08:40:14 +0530

    Update page.tsx

--- client/app/Dashboard/page.tsx
+++ client/app/Dashboard/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { FaClipboardCheck, FaTrashCan } from "react-icons/fa6";
 import { FaEdit } from "react-icons/fa";
-import axios from "axios";
+import axios from "axios"; 
 import { toast } from "react-toastify";
 import { LuLoader } from "react-icons/lu";
 interface UserItem {
